aeb7c828f768d9e1327b0db8bf2942a6dea41a9a
sinha <1233sandeepsinha@gmail.com>
2025-07-06 17:11:14 +0530

    adding the frontend into backend

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -2,13 +2,15 @@ import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 import admin from "firebase-admin";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Load JSON manually
+// Parse and fix private key newlines
 const serviceAccount = JSON.parse(process.env.GOOGLE_CREDENTIALS);
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
