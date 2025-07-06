b3786d8773e243ad6d5f925e1bdd4ac30a846228
sinha <1233sandeepsinha@gmail.com>
2025-07-06 18:53:15 +0530

    adding the frontend into backend

--- frontend/src/utils/axoisInstance.js
+++ frontend/src/utils/axoisInstance.js
@@ -4,7 +4,6 @@ import { BASE_URL } from "./apiPath";
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
   timeout: 10000,
-  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
